a129a4c5cedce09dce05a7f902bafd3823c4f3ea
hyeonji0730 <leehyeonji0730@naver.com>
2023-06-07 06:11:01 +0900

    잠시 수정

---
+++ Assets/WeaponRevolver.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRevolver : WeaponBase
+{
+    // Start is called before the first frame update
+    /*public override void StartWeaponAction(int type= 0 )
+    {
+
+    }
+    public override void StopWeaponAction(int type = 0)
+    {
+
+    }
+    public override void StartReload()
+    {
+
+    }*/
+}
